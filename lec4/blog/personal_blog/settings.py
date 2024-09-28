c98cecf332a728cbed23e58dad9ff16cfbbff5ca
Shiqi <cuisq0418@gmail.com>
2024-09-27 22:14:53 -0400

    Add css file to make pages look better; add requirements.txt.

--- lec4/blog/personal_blog/settings.py
+++ lec4/blog/personal_blog/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]  # Optional: if static folder is at the project level
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
